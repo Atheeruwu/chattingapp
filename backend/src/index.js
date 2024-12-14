af4ed6c26bb85d97a389837b72731fba8a6ad184
atheer <atheer@example.com>
2024-12-14 11:53:58 +0300

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+
+// Import custom modules
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+// Load environment variables
+dotenv.config();
+
+const PORT = process.env.PORT || 5002;  // Default to 5002 if PORT is not set in .env
+const __dirname = path.resolve();
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Frontend URL
+    credentials: true,
+  })
+);
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+// Start the server
+server.listen(PORT, () => {
+  console.log("Server is running on PORT:" + PORT);
+  connectDB();  // Connect to MongoDB
+});
